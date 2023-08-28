646c64859c01c7cd4e0ad0a51f90404e0df15a41
Andrey Zharikov <andrey@zharikov.pro>
2023-08-28 17:56:24 +0600

    autofocus on textarea for create note drawer

--- src/components/notes/create-note.tsx
+++ src/components/notes/create-note.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import {Input, Drawer, Button, Space} from "antd";
 import Note from "../../lib/rrrighter/src/note";
+import {TextAreaRef} from "antd/es/input/TextArea";
 
 const { TextArea } = Input;
 
 export default function CreateNote(props: { note: Note, onCancel: Function, onCreate: Function }) {
     const [text, setText] = React.useState(props.note.text)
+    const inputRef = React.createRef<TextAreaRef>()
 
     const onTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setText(e.target.value)
@@ -19,14 +21,18 @@ export default function CreateNote(props: { note: Note, onCancel: Function, onCr
         props.onCreate({ id: props.note.id, text })
     }
 
+    const setFocus = (isOpen: boolean) => {
+        isOpen && inputRef?.current?.focus();
+    };
+
     return (
-        <Drawer title="New note" size={"large"} open={true} onClose={onCancel} extra={
+        <Drawer title="New note" size={"large"} open={true} afterOpenChange={setFocus} onClose={onCancel} extra={
             <Space>
                 <Button onClick={onCancel}>Cancel</Button>
                 <Button type={"primary"} onClick={onCreate}>Create</Button>
             </Space>
         }>
-            <TextArea autoFocus rows={5} value={text} onChange={onTextChange} />
+            <TextArea ref={inputRef} rows={5} value={text} onChange={onTextChange} />
             <p>ID:<span>{props.note.id}</span></p>
         </Drawer>
     )
