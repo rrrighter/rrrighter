e85c1cf2d5f6f60d8231903bb4d9bf5880e715e2
Andrey Zharikov <andrey@zharikov.pro>
2023-09-05 00:37:49 +0600

    feat: `/` search hotkey

--- src/components/notebook/search/search.tsx
+++ src/components/notebook/search/search.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import {Select} from "antd";
 import FormattedText from "../../notes/formatted-text";
 import Notebook from "../../../lib/rrrighter/src/notebook";
+import { useHotkeys } from 'react-hotkeys-hook';
 
 const {Option} = Select;
 
 export default function Search(props: { notebook: Notebook, onSelect: Function }) {
+    useHotkeys('/', (e) => {
+        e.preventDefault()
+        document.getElementById('search')?.focus()
+    })
+
     const searchOptions = Array.from(props.notebook.notes()).map((note) => {
         const firstLine = note.text.split('\n')[0]
         return <Option key={note.id} value={firstLine}>
@@ -14,10 +20,12 @@ export default function Search(props: { notebook: Notebook, onSelect: Function }
     })
 
     return <Select
+        id='search'
+        showAction={['focus', 'click']}
         style={{width: '20em'}}
         showSearch
         bordered={false}
-        placeholder="Search"
+        placeholder="Search (/)"
         value={[]}
         onSelect={(_val: string, option: any) => props.onSelect(option.key)}
     >
