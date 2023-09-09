4c700aeacbeb98a21cc9738af0ccc99271c47a71
Andrey Zharikov <andrey@zharikov.pro>
2023-09-09 19:44:03 +0600

    feat: .rrrighter file format with ordered hierarchs and ordered children

--- src/lib/rrrighter/src/json-persistence/index.test.ts
+++ src/lib/rrrighter/src/json-persistence/index.test.ts
@@ -9,55 +9,35 @@ describe('JSON persistence', () => {
         notebook = new Notebook()
     })
 
-    const grandparent: Note = { id: '1', text: 'grandparent' }
-    const parent: Note = { id: '2', text: 'parent' }
+    const mother: Note = { id: '1', text: 'mother' }
+    const father: Note = { id: '2', text: 'father' }
     const child: Note = { id: '3', text: 'child' }
-
-    const threeLevelHierarchyWithParentsJsonObject = {
-        // Having top-level "notes" key makes it extendable in future versions
-        // which may add more keys like "author", "license", "drafts", "sops", etc.
-        //
-        // Storing notes' parents instead of children
-        // makes it harder to confuse the structure with a tree.
-        // It clearly shows that nodes may have multiple parents.
-        //
-        // It also makes it easier to edit notebook manually
-        // (e.g. to move a note to another parent).
-        'notes': {
-            '1': { text: 'grandparent' },
-            '2': { text: 'parent', parents: ['1'] },
-            '3': { text: 'child', parents: ['2'] }
-        }
-    }
+    const grandchild: Note = { id: '4', text: 'grandchild' }
 
     const threeLevelHierarchyWithChildrenJsonObject = {
         // Having top-level "notes" key makes it extendable in future versions
         // which may add more keys like "author", "license", "drafts", "sops", etc.
-        //
-        // Storing notes' parents instead of children
-        // makes it harder to confuse the structure with a tree.
-        // It clearly shows that nodes may have multiple parents.
-        //
-        // It also makes it easier to edit notebook manually
-        // (e.g. to move a note to another parent).
-        'notes': {
-            '1': { id: '1', text: 'grandparent', children: ['2'] },
-            '2': { id: '2', text: 'parent', children: ['3'] },
-            '3': { id: '3', text: 'child' }
-        }
+        'notes': [
+            { id: '1', text: 'mother', children: ['3'] },
+            { id: '2', text: 'father', children: ['3'] },
+            { id: '3', text: 'child', children: ['4'] },
+            { id: '4', text: 'grandchild' }
+        ]
     }
 
     describe('.toJsonObject()', () => {
         test('Converts empty notebook to JSON object', () => {
-            expect(toJsonObject(notebook)).toStrictEqual({ notes: {} })
+            expect(toJsonObject(notebook)).toStrictEqual({ notes: [] })
         })
 
         test('Converts hierarchical notebook to JSON object', () => {
-            notebook.upsert(grandparent)
-            notebook.upsert(parent)
+            notebook.upsert(mother)
+            notebook.upsert(father)
             notebook.upsert(child)
-            notebook.attach(grandparent.id, parent.id)
-            notebook.attach(parent.id, child.id)
+            notebook.upsert(grandchild)
+            notebook.attach(mother.id, child.id)
+            notebook.attach(father.id, child.id)
+            notebook.attach(child.id, grandchild.id)
             expect(toJsonObject(notebook)).toStrictEqual(threeLevelHierarchyWithChildrenJsonObject)
         })
 
@@ -66,13 +46,8 @@ describe('JSON persistence', () => {
 
     describe('.fromJsonObject()', () => {
         test('Converts empty JSON object to empty notebook', () => {
-            const notebook = fromJsonObject({ notes: {} })
+            const notebook = fromJsonObject({ notes: [] })
             expect(Array.from(notebook.notes())).toStrictEqual([])
-        })
-
-        test('Converts JSON object with parents to hierarchical notebook', () => {
-            notebook = fromJsonObject(threeLevelHierarchyWithParentsJsonObject)
-            expect(toJsonObject(notebook)).toStrictEqual(threeLevelHierarchyWithChildrenJsonObject)
         })
 
         test('Converts JSON object with children to hierarchical notebook', () => {
--- src/lib/rrrighter/src/json-persistence/index.ts
+++ src/lib/rrrighter/src/json-persistence/index.ts
@@ -1,30 +1,21 @@
 import Notebook from "./../notebook"
-import Note from "./../note"
+
+type NoteJson = { id: string, text: string, children?: string[] }
 
 export type NotebookJson = {
-    notes: {
-        // TODO: deprecate parents once converted all old notebooks
-        [key: string]: { id?: string, text: string, parents?: string[], children?: string[] }
-    }
+    notes: NoteJson[]
 }
 
 export const fromJsonObject = (jsonObject: NotebookJson): Notebook => {
     let notebook = new Notebook()
 
-    let notes: { [key: string]: Note } = {}
-    for(const id in jsonObject.notes) {
-        notes[id] = { id, text: jsonObject.notes[id].text }
-        notebook.upsert(notes[id])
+    for(const note of jsonObject.notes) {
+        notebook.upsert({ id: note.id, text: note.text })
     }
 
-    for(const childId in jsonObject.notes) {
-        for(const parentId of jsonObject.notes[childId].parents || []) {
-            notebook.attach(parentId, childId)
-        }
-    }
-
-    for(const parentId in jsonObject.notes) {
-        for(const childId of jsonObject.notes[parentId].children || []) {
+    for(const note of jsonObject.notes) {
+        const parentId = note.id
+        for(const childId of note.children || []) {
             notebook.attach(parentId, childId)
         }
     }
@@ -33,14 +24,16 @@ export const fromJsonObject = (jsonObject: NotebookJson): Notebook => {
 }
 
 export const toJsonObject = (notebook: Notebook): NotebookJson => {
-    let jsonObject: NotebookJson = { notes: {} }
+    let notes: NoteJson[] = []
+
     notebook.notes().forEach(note => {
+        const noteJson: NoteJson = { id: note.id, text: note.text }
         const children = notebook.children(note.id)
-        jsonObject.notes[note.id] = { id: note.id, text: note.text }
         if (children && children.length > 0) {
-            jsonObject.notes[note.id].children = Array.from(children).map(child => child.id)
+            noteJson.children = children.map(child => child.id)
         }
+        notes.push(noteJson)
     })
 
-    return jsonObject
+    return { notes }
 }
